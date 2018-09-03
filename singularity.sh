f4f46ef99208bdec57845bd7e5c0686d5ebf683f
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-03 12:17:02 +0200

    move build requirements

--- singularity.sh
+++ singularity.sh
@@ -10,16 +10,13 @@ echo $SINGULARITY_DIR
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then # use homebrew version
 	echo "Panic!"
 else
-	# install squashfs
+	# install build requirements
 	sudo apt-get update
-	sudo apt-get install -y squashfs-tools
+	sudo apt-get install -y squashfs-tools libarchive-dev build-essential
 
 	if [ -f $SINGULARITY_DIR/bin/singularity ]; then
 		echo "using cached build"
 	else
-		# install build requirements
-		sudo apt-get install -y build-essential libarchive-dev
-		
 		# download singularity
 		pushd /tmp
 		wget "https://github.com/singularityware/singularity/releases/download/${SINGULARITY_VERSION}/singularity-${SINGULARITY_VERSION}.tar.gz"
