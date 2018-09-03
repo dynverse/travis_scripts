6e1560d84804a6d0a1e79abd3f93e9875a5853cd
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-03 10:57:08 +0200

    fix scripts

--- test_docker_image.sh
+++ test_docker_image.sh
@@ -12,7 +12,7 @@ source("example.R")
 
 config <- container_docker()
 
-meth <- create_ti_method_with_container("dynverse/travis_test_build", config = config)
+meth <- create_ti_method_with_container("dynverse/travis_test_build", config = config)()
 
 traj <- infer_trajectory(data, meth, params)
 HERE
--- test_singularity_image.sh
+++ test_singularity_image.sh
@@ -26,7 +26,7 @@ source("example.R")
 
 config <- container_singularity()
 
-meth <- create_ti_method_with_container("dynverse/travis_test_build", config = config)
+meth <- create_ti_method_with_container("dynverse/travis_test_build", config = config)()
 
 traj <- infer_trajectory(data, meth, params)
 HERE
